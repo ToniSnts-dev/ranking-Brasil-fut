bb1725d34f031b49cf5269f37a780f9155304082
ToniSnts-dev <etonisantos@gmail.com>
2026-09-25 16:49:22 -0300

    atualizando para busca de estatística de jogadores

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
+import { buscarJogador } from "./services/apiFootball.js";
 import { buscarTime } from "./services/apiFootball.js";
 async function main(){
+    const realMadrid = await buscarTime("Real Madrid");
    
-    const team = await buscarTime("Corinthians");
-
-    if(!team){
+    if(!realMadrid){
         return;
     }
-
-    console.log("Id: ", team.id);
-    console.log("Nome: ", team.name);
-    console.log("País: ", team.country);
-    console.log("Fundação: ", team.founded);
+    await buscarJogador("Vinicius Junior", realMadrid.id);
 }
-
+    
 main();
--- src/services/apiFootball.ts
+++ src/services/apiFootball.ts
@@ -1,6 +1,32 @@
+import type { Player } from "../types/Player.js";
 import type { Team } from "../types/Team.js";
 
 const apiKey = process.env.API_FOOTBALL_KEY;
+
+export async function buscarJogador(nome: string, teamId: number){
+    if (!apiKey){
+        console.error("API_FOOTBALL_KEY não encontrada");
+        return;
+    }
+
+    const response = await fetch(
+        `https://v3.football.api-sports.io/players?search=${encodeURIComponent(nome)}&team=${teamId}&season=2024`,
+        {
+            headers: {
+                "x-apisports-key": apiKey
+            }
+        }
+    );
+
+    if(!response.ok){
+        console.error("Erro HTTP:", response.status);
+        return;
+    }
+
+    const data = await response.json();
+
+    console.dir(data.response[0], {depth: null});
+}
 
 export async function buscarTime(nome: string){
     const response = await fetch(
